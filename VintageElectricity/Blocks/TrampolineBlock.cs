95439011f289959fe029947268294d39c0a0c618
KrykiZZ <krykizzzz@gmail.com>
2022-08-04 00:32:49 +0300

    Add project files.

---
+++ VintageElectricity/Blocks/TrampolineBlock.cs
@@ -0,0 +1,20 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.MathTools;
+
+namespace VintageElectricity.Blocks
+{
+    public class TrampolineBlock : Block
+    {
+        private AssetLocation _tickSound = new AssetLocation("game", "tick");
+
+        public override void OnEntityCollide(IWorldAccessor world, Entity entity, BlockPos pos, BlockFacing facing, Vec3d collideSpeed, bool isImpact)
+        {
+            if (isImpact && facing.Axis == EnumAxis.Y)
+            {
+                world.PlaySoundAt(_tickSound, entity.Pos.X, entity.Pos.Y, entity.Pos.Z);
+                entity.Pos.Motion.Y *= -0.8;
+            }
+        }
+    }
+}
---
+++ VintageElectricity/VintageElectricity.cs
@@ -0,0 +1,14 @@
+﻿using VintageElectricity.Blocks;
+using Vintagestory.API.Common;
+
+namespace VintageElectricity
+{
+    public class VintageElectricity : ModSystem
+    {
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+            api.RegisterBlockClass("trampoline", typeof(TrampolineBlock));
+        }
+    }
+}
